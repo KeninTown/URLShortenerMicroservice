53f7f4ea745d675a3d4b95eea30d853a7aca171b
Alexander <soldatovalex207z@gmail.com>
2023-05-06 12:58:53 +0300

    done without MongoDB

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const mongoose = require('mongoose');
 
+const app = express();
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', (req, res) => {
   res.json({ greeting: 'hello API' });
+});
+
+//Номер shortUrl, который соответствует конкретному url
+const shortUrls = {};
+let shortUrlId = 1;
+
+app.post('/api/shorturl', (req, res) => {
+  let url = new URL(req.body.url);
+  let host = url.hostname;
+  dns.lookup(host, (err) => {
+    if (err || host === '') {
+      res.json({ error: 'Invalid URL' });
+    }
+    else {
+      shortUrls[shortUrlId] = url;
+      shortUrlId++;
+      res.json({ original_url: url, short_url: shortUrlId - 1 });
+    }
+  })
+});
+
+app.get('/api/shorturl/:index', (req, res) => {
+  let shortUrl = req.params.index;
+  if (shortUrls.hasOwnProperty(shortUrl))
+    res.redirect(shortUrls[shortUrl]);
 });
 
 app.listen(port, function() {
